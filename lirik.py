113fa8027a4cdb0d154da6cb55816757f41033b0
lannzeverfall's <wulanarmaniska@gmail.com>
2024-10-11 22:09:19 +0700

    Update lirik.py
    done

--- lirik.py
+++ lirik.py
@@ -4,11 +4,10 @@ import sys
 
 def print_lirik():
     line = [
-        ("apa sudah ada kabar lain yang kau tunggu?",0.10),
-        ("sudah adakah yang gantikan ku?",0.09),
-        ("yang khawatirkan mu setiap waktu",0.07),
-        ("yang cerita tentang apapun sampai hal-hal tak perlu",0.10),
-        ]
+        ("sudahlah kali ini aku kalah",0.10),
+        ("kehilangan mahkota",0.09),
+        ("kau dan dia pemenang nya,0.07),]
+        
     delays=[2, 2.3, 3.2, 1,] 
     
     for i, (line, char_delay) in enumerate(line):
