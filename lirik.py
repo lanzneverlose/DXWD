3f65832d9b7b6d53471c9cb5d51cae1240539b4d
lannzeverfall's <wulanarmaniska@gmail.com>
2024-10-11 22:14:50 +0700

    Update lirik.py
    p

--- lirik.py
+++ lirik.py
@@ -4,10 +4,10 @@ import sys
 
 def print_lirik():
     line = [
-        ("apa sudah ada kabar lain yang kau tunggu?",0.10),
-        ("sudah adakah yang gantikan ku?",0.09),
-        ("yang khawatirkan mu setiap waktu",0.07),
-        ("yang cerita tentang apapun sampai hal-hal tak perlu",0.10),
+        ("sudahlah kali ini aku kalah",0.10),
+        ("kehilangan mahkota",0.09),
+        ("kau dan dia",0.07),
+        ("pemenangnya",0.10),
         ]
     delays=[2, 2.3, 3.2, 1,] 
     
